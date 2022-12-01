9bab8028318891292d3081273968c0234d6eddb7
GMICHON <gauthier.michon@ynov.com>
2022-12-01 16:26:56 +0100

    end Creation Detail Pages

--- app/src/main/java/com/notag/pokedex/DetailMovesActivity.kt
+++ app/src/main/java/com/notag/pokedex/DetailMovesActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.notag.pokedex.models.Abilities
+import com.notag.pokedex.models.Moves
 
 class DetailMovesActivity : AppCompatActivity() {
 
-    private val textViewAbilityName: TextView by lazy { findViewById<TextView>(R.id.textViewAbilityName) }
-    private val textViewAbilityContent: TextView by lazy { findViewById<TextView>(R.id.textViewAbilityContent) }
+    private val textViewMoveName: TextView by lazy { findViewById<TextView>(R.id.textViewMoveName) }
+    private val textViewMoveContent: TextView by lazy { findViewById<TextView>(R.id.textViewMoveContent) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_abilities)
+        setContentView(R.layout.activity_detail_moves)
 
-        val move = intent.getParcelableExtra<Abilities>("move")
+        val move = intent.getParcelableExtra<Moves>("move")
+
+        textViewMoveName.text = move?.name
+        textViewMoveContent.text = move?.flavor_text_entries?.get(0)?.flavor_text
     }
 }
